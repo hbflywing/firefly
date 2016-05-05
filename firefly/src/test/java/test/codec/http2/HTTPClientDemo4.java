7bbcb7bed0812993b4f2dd42084f3a7ec9edde91
Alvin Qiu <qptkk@163.com>
2016-05-05 16:44:10 +0800

    test

--- firefly/src/test/java/test/codec/http2/HTTPClientDemo4.java
+++ firefly/src/test/java/test/codec/http2/HTTPClientDemo4.java
@@ -1,6 +1,7 @@
 package test.codec.http2;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Phaser;
@@ -11,55 +12,91 @@ import com.firefly.client.http2.HTTP2Client;
 import com.firefly.client.http2.HTTPClientConnection;
 import com.firefly.client.http2.HTTPClientRequest;
 import com.firefly.codec.http2.model.HttpVersion;
+import com.firefly.codec.http2.model.MetaData.Request;
+import com.firefly.codec.http2.model.MetaData.Response;
 import com.firefly.codec.http2.stream.HTTP2Configuration;
 import com.firefly.codec.http2.stream.HTTPConnection;
+import com.firefly.codec.http2.stream.HTTPOutputStream;
 import com.firefly.utils.concurrent.FuturePromise;
+import com.firefly.utils.function.Action6;
+import com.firefly.utils.function.Func4;
+import com.firefly.utils.function.Func5;
 import com.firefly.utils.io.BufferUtils;
 
 public class HTTPClientDemo4 {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
-//		final HTTP2Configuration http2Configuration = new HTTP2Configuration();
-//		HTTP2Client client = new HTTP2Client(http2Configuration);
-//		FuturePromise<HTTPClientConnection> promise = new FuturePromise<>();
-//		client.connect("localhost", 7777, promise);
-//		HTTPConnection connection = promise.get();
-//		if (connection.getHttpVersion() == HttpVersion.HTTP_1_1) {
-//			final Phaser phaser = new Phaser(2);
-//			HTTP1ClientConnection http1ClientConnection = (HTTP1ClientConnection) connection;
-//
-//			HTTPClientRequest request = new HTTPClientRequest("GET", "/index?version=1&test=ok");
-//			http1ClientConnection.send(request,
-//					new ClientHTTPHandler.Adapter().messageComplete((req, resp, outputStream, conn) -> {
-//						System.out.println("message complete: " + resp.getStatus() + "|" + resp.getReason());
-//						phaser.arrive();
-//						return true;
-//					}).content((buffer, req, resp, outputStream, conn) -> {
-//						System.out.println(BufferUtils.toString(buffer, StandardCharsets.UTF_8));
-//						return false;
-//					}).badMessage((errCode, reason, req, resp, outputStream, conn) -> {
-//						System.out.println("error: " + errCode + "|" + reason);
-//					}));
-//			phaser.arriveAndAwaitAdvance();
-//			
-//			HTTPClientRequest request2 = new HTTPClientRequest("GET", "/test");
-//			http1ClientConnection.send(request2,
-//					new ClientHTTPHandler.Adapter().messageComplete((req, resp, outputStream, conn) -> {
-//						System.out.println("message complete: " + resp.getStatus() + "|" + resp.getReason());
-//						phaser.arrive();
-//						return true;
-//					}).content((buffer, req, resp, outputStream, conn) -> {
-//						System.out.println(BufferUtils.toString(buffer, StandardCharsets.UTF_8));
-//						return false;
-//					}).badMessage((errCode, reason, req, resp, outputStream, conn) -> {
-//						System.out.println("error: " + errCode + "|" + reason);
-//						phaser.arrive();
-//					}));
-//			phaser.arriveAndAwaitAdvance();
-//			
-//			System.out.println("demo4 request finished");
-//			http1ClientConnection.close();
-//		}
+		final HTTP2Configuration http2Configuration = new HTTP2Configuration();
+		HTTP2Client client = new HTTP2Client(http2Configuration);
+		FuturePromise<HTTPClientConnection> promise = new FuturePromise<>();
+		client.connect("localhost", 7777, promise);
+		HTTPConnection connection = promise.get();
+		if (connection.getHttpVersion() == HttpVersion.HTTP_1_1) {
+			final Phaser phaser = new Phaser(2);
+			HTTP1ClientConnection http1ClientConnection = (HTTP1ClientConnection) connection;
+
+			HTTPClientRequest request = new HTTPClientRequest("GET", "/index?version=1&test=ok");
+			http1ClientConnection.send(request, new ClientHTTPHandler.Adapter()
+					.messageComplete(new Func4<Request, Response, HTTPOutputStream, HTTPConnection, Boolean>() {
+
+						@Override
+						public Boolean call(Request request, Response resp, HTTPOutputStream outputStream,
+								HTTPConnection connection) {
+							System.out.println("message complete: " + resp.getStatus() + "|" + resp.getReason());
+							phaser.arrive();
+							return true;
+						}
+					}).content(new Func5<ByteBuffer, Request, Response, HTTPOutputStream, HTTPConnection, Boolean>() {
+
+						@Override
+						public Boolean call(ByteBuffer buffer, Request t2, Response t3, HTTPOutputStream t4,
+								HTTPConnection t5) {
+							System.out.println(BufferUtils.toString(buffer, StandardCharsets.UTF_8));
+							return false;
+						}
+
+					}).badMessage(new Action6<Integer, String, Request, Response, HTTPOutputStream, HTTPConnection>() {
+
+						@Override
+						public void call(Integer errCode, String reason, Request t3, Response t4, HTTPOutputStream t5,
+								HTTPConnection t6) {
+							System.out.println("error: " + errCode + "|" + reason);
+						}
+
+					}));
+			phaser.arriveAndAwaitAdvance();
+
+			HTTPClientRequest request2 = new HTTPClientRequest("GET", "/test");
+			http1ClientConnection.send(request2, new ClientHTTPHandler.Adapter()
+					.messageComplete(new Func4<Request, Response, HTTPOutputStream, HTTPConnection, Boolean>() {
+						@Override
+						public Boolean call(Request request, Response resp, HTTPOutputStream outputStream,
+								HTTPConnection connection) {
+							System.out.println("message complete: " + resp.getStatus() + "|" + resp.getReason());
+							phaser.arrive();
+							return true;
+						}
+					}).content(new Func5<ByteBuffer, Request, Response, HTTPOutputStream, HTTPConnection, Boolean>() {
+						@Override
+						public Boolean call(ByteBuffer buffer, Request t2, Response t3, HTTPOutputStream t4,
+								HTTPConnection t5) {
+							System.out.println(BufferUtils.toString(buffer, StandardCharsets.UTF_8));
+							return false;
+						}
+					}).badMessage(new Action6<Integer, String, Request, Response, HTTPOutputStream, HTTPConnection>() {
+
+						@Override
+						public void call(Integer errCode, String reason, Request t3, Response t4, HTTPOutputStream t5,
+								HTTPConnection t6) {
+							System.out.println("error: " + errCode + "|" + reason);
+							phaser.arrive();
+						}
+					}));
+			phaser.arriveAndAwaitAdvance();
+
+			System.out.println("demo4 request finished");
+			http1ClientConnection.close();
+		}
 	}
 
 }
--- firefly/src/test/java/test/codec/http2/HTTPServerDemo4.java
+++ firefly/src/test/java/test/codec/http2/HTTPServerDemo4.java
@@ -9,82 +9,92 @@ import java.util.List;
 import com.firefly.codec.http2.model.HttpHeader;
 import com.firefly.codec.http2.model.HttpHeaderValue;
 import com.firefly.codec.http2.model.HttpURI;
+import com.firefly.codec.http2.model.MetaData.Request;
+import com.firefly.codec.http2.model.MetaData.Response;
 import com.firefly.codec.http2.stream.HTTP2Configuration;
+import com.firefly.codec.http2.stream.HTTPConnection;
 import com.firefly.codec.http2.stream.HTTPOutputStream;
 import com.firefly.server.http2.HTTP2Server;
 import com.firefly.server.http2.ServerHTTPHandler;
 import com.firefly.utils.collection.MultiMap;
+import com.firefly.utils.function.Func4;
 import com.firefly.utils.io.BufferUtils;
 
 public class HTTPServerDemo4 {
 
 	public static void main(String[] args) {
-//		int length = 2500;
-//		StringBuilder s = new StringBuilder(length);
-//		for (int i = 0; i < length; i++) {
-//			s.append('t');
-//		}
-//		final String data = s.toString();
-//
-//		HTTP2Configuration http2Configuration = new HTTP2Configuration();
-//		HTTP2Server server = new HTTP2Server("localhost", 7777, http2Configuration,
-//				new ServerHTTPHandler.Adapter().messageComplete((request, response, outputStream, connection) -> {
-//
-//					HttpURI uri = request.getURI();
-//					// System.out.println("current path is " + uri.getPath());
-//					// System.out.println("current parameter string is " +
-//					// uri.getQuery());
-//					// System.out.println("current http headers are " +
-//					// request.getFields());
-//					MultiMap<String> parameterMap = new MultiMap<String>();
-//					uri.decodeQueryTo(parameterMap);
-//					// System.out.println("current parameters are " +
-//					// parameterMap);
-//
-//					if (uri.getPath().equals("/test")) {
-//						response.setStatus(200);
-//						response.setHttpVersion(request.getVersion());
-//						response.getFields().add(HttpHeader.CONNECTION, HttpHeaderValue.KEEP_ALIVE);
-//						try (HTTPOutputStream output = outputStream) {
-//							output.writeWithContentLength(BufferUtils.toBuffer(data, StandardCharsets.UTF_8));
-//						} catch (IOException e) {
-//							e.printStackTrace();
-//						}
-//					} else if (uri.getPath().equals("/index")) {
-//						response.setStatus(200);
-//
-//						List<ByteBuffer> list = new ArrayList<>();
-//						list.add(BufferUtils.toBuffer("hello the server demo ", StandardCharsets.UTF_8));
-//						list.add(BufferUtils.toBuffer("test chunk 1 ", StandardCharsets.UTF_8));
-//						list.add(BufferUtils.toBuffer("test chunk 2 ", StandardCharsets.UTF_8));
-//						list.add(BufferUtils.toBuffer("中文的内容，哈哈 ", StandardCharsets.UTF_8));
-//						list.add(BufferUtils.toBuffer("靠！！！ ", StandardCharsets.UTF_8));
-//
-//						try (HTTPOutputStream output = outputStream) {
-//							output.writeWithContentLength(list.toArray(BufferUtils.EMPTY_BYTE_BUFFER_ARRAY));
-//						} catch (IOException e) {
-//							e.printStackTrace();
-//						}
-//					} else if (uri.getPath().equals("/testContinue")) {
-//						response.setStatus(200);
-//						try (HTTPOutputStream output = outputStream) {
-//							output.writeWithContentLength(
-//									BufferUtils.toBuffer("receive Continue-100 successfully ", StandardCharsets.UTF_8));
-//						} catch (IOException e) {
-//							e.printStackTrace();
-//						}
-//					} else {
-//						response.setStatus(404);
-//						try (HTTPOutputStream output = outputStream) {
-//							output.writeWithContentLength(BufferUtils.toBuffer("找不到页面", StandardCharsets.UTF_8));
-//						} catch (IOException e) {
-//							e.printStackTrace();
-//						}
-//					}
-//
-//					return true;
-//				}));
-//		server.start();
+		int length = 2500;
+		StringBuilder s = new StringBuilder(length);
+		for (int i = 0; i < length; i++) {
+			s.append('t');
+		}
+		final String data = s.toString();
+
+		HTTP2Configuration http2Configuration = new HTTP2Configuration();
+		HTTP2Server server = new HTTP2Server("localhost", 7777, http2Configuration, new ServerHTTPHandler.Adapter()
+				.messageComplete(new Func4<Request, Response, HTTPOutputStream, HTTPConnection, Boolean>() {
+
+					@Override
+					public Boolean call(Request request, Response response, HTTPOutputStream outputStream,
+							HTTPConnection connection) {
+						HttpURI uri = request.getURI();
+						// System.out.println("current path is " +
+						// uri.getPath());
+						// System.out.println("current parameter string is " +
+						// uri.getQuery());
+						// System.out.println("current http headers are " +
+						// request.getFields());
+						MultiMap<String> parameterMap = new MultiMap<String>();
+						uri.decodeQueryTo(parameterMap);
+						// System.out.println("current parameters are " +
+						// parameterMap);
+
+						if (uri.getPath().equals("/test")) {
+							response.setStatus(200);
+							response.setHttpVersion(request.getVersion());
+							response.getFields().add(HttpHeader.CONNECTION, HttpHeaderValue.KEEP_ALIVE);
+							try (HTTPOutputStream output = outputStream) {
+								output.writeWithContentLength(BufferUtils.toBuffer(data, StandardCharsets.UTF_8));
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						} else if (uri.getPath().equals("/index")) {
+							response.setStatus(200);
+
+							List<ByteBuffer> list = new ArrayList<>();
+							list.add(BufferUtils.toBuffer("hello the server demo ", StandardCharsets.UTF_8));
+							list.add(BufferUtils.toBuffer("test chunk 1 ", StandardCharsets.UTF_8));
+							list.add(BufferUtils.toBuffer("test chunk 2 ", StandardCharsets.UTF_8));
+							list.add(BufferUtils.toBuffer("中文的内容，哈哈 ", StandardCharsets.UTF_8));
+							list.add(BufferUtils.toBuffer("靠！！！ ", StandardCharsets.UTF_8));
+
+							try (HTTPOutputStream output = outputStream) {
+								output.writeWithContentLength(list.toArray(BufferUtils.EMPTY_BYTE_BUFFER_ARRAY));
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						} else if (uri.getPath().equals("/testContinue")) {
+							response.setStatus(200);
+							try (HTTPOutputStream output = outputStream) {
+								output.writeWithContentLength(BufferUtils.toBuffer("receive Continue-100 successfully ",
+										StandardCharsets.UTF_8));
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						} else {
+							response.setStatus(404);
+							try (HTTPOutputStream output = outputStream) {
+								output.writeWithContentLength(BufferUtils.toBuffer("找不到页面", StandardCharsets.UTF_8));
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+
+						return true;
+					}
+
+				}));
+		server.start();
 
 	}
 
